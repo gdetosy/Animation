ff1cf7407dbbbc63bbd1d1e961866b47f0cb53d5
gdetosy <tosyjdm@gmail.com>
2022-12-16 15:30:05 +0300

    first animation

---
+++ Animation/Animation/AnimationEnum.swift
@@ -0,0 +1,43 @@
+//
+//  AnimationEnum.swift
+//  Animation
+//
+//  Created by tosy on 16.12.22.
+//
+
+import Foundation
+enum Animations: String {
+    case shake
+    case pop
+    case morph
+    case squeeze
+    case wobble
+    case swing
+    case flipX
+    case flipY
+    case fall
+    case squeezeLeft
+    case squeezeRight
+    case squeezeDown
+    case squeezeUp
+    case slideLeft
+    case slideRight
+    case slideDown
+    case slideUp
+    case fadeIn
+    case fadeOut
+    case fadeInLeft
+    case fadeInRight
+    case fadeInDown
+    case fadeInUp
+    case zoomIn
+    case zoomOut
+    case flash
+}
+enum Curves: String {
+    case spring
+    case linear
+    case easeIn
+    case easeOut
+    case easeInOut
+}
--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startBtn: SpringButton!
+    @IBOutlet weak var views: SpringView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        views.delay = 3
+      
+        views.animation = Animations.fadeIn.rawValue
+        views.animate()
+        
+        
+        
     }
 
 
+    @IBAction func startAnimation(_ sender: Any) {
+        startBtn.animation = Animations.shake.rawValue
+        startBtn.animate()
+        views.animation = Animations.zoomIn.rawValue
+        views.animate()
+        
+    }
 }
 
